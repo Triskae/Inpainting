1a90af6fbd2863dccc8c4c66c01883d8b052372f
Unknown <sorel.nadaud@outlook.com>
2017-09-12 12:14:55 +0200

    Commit #2
    Constructeur Color

--- src/topology/Color.java
+++ src/topology/Color.java
@@ -2,36 +2,35 @@ package topology;
 
 
 public class Color{ 			// Color (B,G,R)
-
-
     public byte[] val;
 
+    //Constructeurs
+    public Color(byte b1 ,byte b2 ,byte b3) {
+        val[0] = b1;
+        val[1] = b2;
+        val[2] = b3;
+    }
 
+    public Color(int c1,int c2 ,int c3) {
+
+
+    }
+
+    //Fonctions
     public static int dist(Color c1 ,Color c2) {
 
         return 0;
     }
 
-
     public void set(Color c1) {
 
     }
-
 
     boolean isequalto(Color c1) {
 
         return true;
     }
 
-
-    public Color(byte b1 ,byte b2 ,byte b3) {
-
-    }
-
-
-    public Color(int c1,int c2 ,int c3) {
-
-    }
 
 
     @Override public String toString() {
